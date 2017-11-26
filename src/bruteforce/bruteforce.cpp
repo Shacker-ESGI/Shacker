75cabec7d361d740fd8ec6dacce9c4a935214bb6
Aurelien Duval <aurelien.duval6@gmail.com>
2017-11-26 17:53:08 +0100

    Modified parallelizer strategy from range-based to jump-based

--- src/bruteforce/bruteforce.cpp
+++ src/bruteforce/bruteforce.cpp
@@ -15,59 +15,70 @@ PasswordCracker::PasswordCracker(const unsigned char* password_hash,
                                   const unsigned int max_thread_number):
                                   password_hash(password_hash),
                                   possible_keys(possible_keys),
-                                  max_thread_number(max_thread_number) {
+                                  max_thread_number(max_thread_number),
+                                  is_password_found(false),
+                                  increment(0),
+                                  left_bound_index(0)
+                                  {
 }
 
-void PasswordCracker::calculate_buffer_load_shares() {
-  int available_shares = this->max_thread_number;
-  const int keys_length = this->possible_keys.length();
+void PasswordCracker::configure_increment() {
+  unsigned int available_shares = this->max_thread_number;
+  const unsigned int keys_length = this->possible_keys.length();
 
-  while(available_shares >= 1) {
-    this->load_shares_indexes.push_back(std::min(available_shares, keys_length));
+  while(available_shares >= keys_length) {
+    ++this->left_bound_index;
     available_shares /= keys_length;
   }
-}
-
-inline unsigned int PasswordCracker::get_left_bound_from_thread_id_and_index(unsigned int thread_id, unsigned int curr_index) {
-  unsigned int result = std::ceil(thread_id * this->possible_keys.length() / this->load_shares_indexes[curr_index]);
-  unsigned int keys_length = this->possible_keys.length();
-  return result < keys_length ? result : 0;
-}
-
-inline unsigned int PasswordCracker::get_right_bound_from_thread_id_and_index(unsigned int thread_id, unsigned int curr_index) {
-  unsigned int result = std::ceil((thread_id + 1) * this->possible_keys.length() / this->load_shares_indexes[curr_index]);
-  unsigned int keys_length = this->possible_keys.length();
-  return std::min(result, keys_length);
+  this->increment = available_shares;
 }
 
 inline void PasswordCracker::check_password(const unsigned int thread_id) {
-  std::string buffer = " ";
-  std::vector<unsigned int> buffer_indexes;
-  unsigned int curr_index = 0;
+  std::string buffer = "";
+  std::vector<unsigned int> buffer_indexes{0};
 
-  buffer_indexes.push_back(this->get_left_bound_from_thread_id_and_index(thread_id, curr_index));
+  this->init_buffer(buffer, buffer_indexes, thread_id);
 
   while(!this->is_password_found) {
-
-      if(buffer_indexes[curr_index] >= this->get_right_bound_from_thread_id_and_index(thread_id, curr_index)) {
-          this->pass_next_character(thread_id, buffer, buffer_indexes, curr_index);
-      }
-      else {
-          this->try_password(buffer, buffer_indexes, curr_index);
-      }
+    this->try_password(buffer, buffer_indexes);
+    this->update_buffer(buffer, buffer_indexes);
   }
 }
 
-inline void PasswordCracker::try_password(std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int& curr_index) {
-  buffer[curr_index] = this->possible_keys[buffer_indexes[curr_index]];
+void PasswordCracker::update_buffer(std::string& buffer, std::vector<unsigned int>& buffer_indexes) {
+  unsigned int increment = this->increment;
 
-  const bool are_password_same = sha256_compare(buffer, this->password_hash);
+  for(unsigned int i = this->left_bound_index ; increment != 0 ; ++i) {
+    if(i >= buffer_indexes.size()) {
+      this->rescale_context(buffer, buffer_indexes);
+      --increment;
+    }
 
-  ++buffer_indexes[curr_index];
-
-  if(curr_index > 0) {
-      --curr_index;
+    this->update_buffer_at_index(buffer, buffer_indexes, increment, i);
   }
+}
+
+void PasswordCracker::init_buffer(std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int increment) {
+  for(register unsigned int i = 0 ; i == 0 || increment != 0 ; ++i) {
+    if(i >= buffer_indexes.size()) {
+      this->rescale_context(buffer, buffer_indexes);
+    }
+
+    this->update_buffer_at_index(buffer, buffer_indexes, increment, i);
+  }
+}
+
+inline void PasswordCracker::update_buffer_at_index(std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int& increment, unsigned int index) {
+  const unsigned int keys_length = this->possible_keys.length();
+  buffer_indexes[index] += increment;
+
+  increment = buffer_indexes[index] / keys_length;
+  buffer_indexes[index] %= keys_length;
+  buffer[index] = this->possible_keys[buffer_indexes[index]];
+}
+
+inline void PasswordCracker::try_password(std::string& buffer, std::vector<unsigned int>& buffer_indexes) {
+  const bool are_password_same = sha256_compare(buffer, this->password_hash);
 
   if (are_password_same) {
       this->is_password_found = true;
@@ -75,27 +86,16 @@ inline void PasswordCracker::try_password(std::string& buffer, std::vector<unsig
   }
 }
 
-inline void PasswordCracker::pass_next_character(const unsigned int thread_id, std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int& curr_index){
-  buffer_indexes[curr_index] = this->get_left_bound_from_thread_id_and_index(thread_id, curr_index);
-  ++curr_index;
-
-  if(curr_index >= buffer.length()) {
-    this->rescale_context(buffer, buffer_indexes);
-  }
-}
-
 inline void PasswordCracker::rescale_context(std::string& buffer, std::vector<unsigned int>& buffer_indexes) {
-  std::unique_lock<std::mutex> lock(this->mutex);
   buffer_indexes.push_back(0);
-  this->load_shares_indexes.push_back(1);
-  buffer += " ";
+  buffer += this->possible_keys[0];
 }
 
 std::string PasswordCracker::parallel_crack() {
   auto result = this->promise.get_future();
 
   this->is_password_found = false;
-  this->calculate_buffer_load_shares();
+  this->configure_increment();
 
   for(unsigned int i = 0 ; i < this->max_thread_number ; ++i) {
       this->threads.push_back(std::thread(&PasswordCracker::check_password, this, i));
--- src/bruteforce/bruteforce.h
+++ src/bruteforce/bruteforce.h
@@ -25,17 +25,17 @@
         const unsigned int max_thread_number;
         bool is_password_found;
         std::vector<std::thread> threads;
-        std::vector<double> load_shares_indexes;
-        std::mutex mutex;
         std::promise<std::string> promise;
+        unsigned int increment;
+        unsigned int left_bound_index;
 
-        void calculate_buffer_load_shares();
         void check_password(const unsigned int thread_id);
-        inline unsigned int get_left_bound_from_thread_id_and_index(unsigned int thread_id, unsigned int curr_index);
-        inline unsigned int get_right_bound_from_thread_id_and_index(unsigned int thread_id, unsigned int curr_index);
-        inline void try_password(std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int& curr_index);
-        inline void pass_next_character(const unsigned int thread_id, std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int& curr_index);
+        inline void try_password(std::string& buffer, std::vector<unsigned int>& buffer_indexes);
         inline void rescale_context(std::string& buffer, std::vector<unsigned int>& buffer_indexes);
+        void configure_increment();
+        void update_buffer(std::string& buffer, std::vector<unsigned int>& buffer_indexes);
+        void init_buffer(std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int increment);
+        inline void update_buffer_at_index(std::string& buffer, std::vector<unsigned int>& buffer_indexes, unsigned int& increment, unsigned int index);
 
       public:
         PasswordCracker(const unsigned char* password_hash, const std::string possible_keys);
